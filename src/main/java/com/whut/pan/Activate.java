618b39aaf53ce92930403313f38583dd9c78e113
sandeepin <sandeepin@qq.com>
2019-07-06 14:16:54 +0800

    负载均衡

--- src/main/java/com/whut/pan/Activate.java
+++ src/main/java/com/whut/pan/Activate.java
@@ -2,8 +2,10 @@ package com.whut.pan;
 
 import com.whut.pan.service.IPanRestV1Service;
 import hprose.client.HproseHttpClient;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -23,15 +25,34 @@ public class Activate implements ApplicationRunner {
 
     public static IPanRestV1Service panRestV1Service;
 
+    @Value("${panRestV1Host0}")
+    public String panRestV1Host0;
+
+    @Value("${panRestV1Host1}")
+    public String panRestV1Host1;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         LOGGER.info("========== init project start ===========");
-        LOGGER.warn("rpc调用，健康检查返回：{}", activatePanRestV1Service());
+        LOGGER.warn("panRestV1Host0:{} Host1:{}", panRestV1Host0, panRestV1Host1);
+        LOGGER.warn("rpc hprose chealthCheck():{}", activatePanRestV1Service(panRestV1Host0, panRestV1Host1));
         LOGGER.info("==========  init project end  ===========");
     }
 
-    private static boolean activatePanRestV1Service() {
-        CLIENT.useService("http://localhost:32018/api/v1/old");
+    private static boolean activatePanRestV1Service(String host0, String host1) {
+        String[] panRestV1Hosts;
+        if (StringUtils.isEmpty(host0) && StringUtils.isNotEmpty(host1)) {
+            panRestV1Hosts = new String[]{host1};
+        } else if (StringUtils.isNotEmpty(host0) && StringUtils.isEmpty(host1)) {
+            panRestV1Hosts = new String[]{host0};
+        } else {
+            panRestV1Hosts = new String[]{host0, host1};
+        }
+        CLIENT.useService(panRestV1Hosts);
+        // 超时时间
+        CLIENT.setTimeout(30000);
+        // 自动切换服务地址
+        CLIENT.setFailswitch(true);
         try {
             // 通过接口调用
             panRestV1Service = CLIENT.useService(IPanRestV1Service.class);
